0ef02c3ba63d855fe75d8c7b6e329e6a9209ce35
jabu <jabu@gx.ag>
2023-03-03 05:30:07 -0600

    move Triangulator to separate file

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
+pub(crate) const EPSILON: f32 = 1e-7;
+
 #[macro_use]
 mod linked_list;
 mod triangulation;
+mod triangulator;
 mod vec2;
 
-pub use triangulation::{Triangulator, TriangulatorError};
+pub use triangulator::{Triangulator, TriangulatorError};
 
 #[cfg(test)]
 mod test;
--- src/triangulation.rs
+++ src/triangulation.rs
@@ -1,203 +1,6 @@
-use std::{
-    cmp::Ordering,
-    mem::{swap, transmute},
-    pin::Pin,
-    ptr::NonNull,
-};
+use std::{cmp::Ordering, mem::swap, pin::Pin, ptr::NonNull};
 
-use snafu::Snafu;
-
-use crate::{linked_list::LinkedListNode, vec2::Vec2};
-
-const EPSILON: f32 = 1e-7;
-
-#[derive(Default)]
-pub struct Triangulator {
-    contours: Vec<Contour>,
-    total_points: usize,
-}
-
-impl Triangulator {
-    pub fn new() -> Self {
-        Self::default()
-    }
-
-    pub fn add_contour(
-        &mut self,
-        identifier: usize,
-        points: Vec<[f32; 2]>,
-    ) -> Result<(), TriangulatorError> {
-        if points.len() < 3 {
-            return Err(TriangulatorError::Incomplete);
-        }
-
-        self.total_points += points.len();
-        self.contours.push(Contour {
-            identifier,
-            points: unsafe { transmute(points) },
-            is_hole: false,
-            nested_to: None,
-        });
-
-        for contour_index in 0..self.contours.len() {
-            let mut nested_to = None;
-            let is_hole = !ttf_outline_contour_info_majority(
-                self,
-                self.contours[contour_index].identifier,
-                contour_index,
-                &mut nested_to,
-            );
-            self.contours[contour_index].nested_to = nested_to;
-            self.contours[contour_index].is_hole = is_hole;
-        }
-
-        Ok(())
-    }
-
-    pub fn triangulate(&self) -> Result<Vec<[[f32; 2]; 3]>, TriangulatorError> {
-        if !self.contours.is_empty() {
-            let mut mesher = Mesher::new(self);
-            mesher.process(128, true, true)?;
-            Ok(unsafe { transmute(mesher.triangles()) })
-        } else {
-            Err(TriangulatorError::Incomplete)
-        }
-    }
-}
-
-struct Contour {
-    points: Vec<Vec2>,
-    identifier: usize,
-    is_hole: bool,
-    nested_to: Option<usize>,
-}
-
-fn ttf_outline_contour_info_majority(
-    outline: &Triangulator,
-    identifier: usize,
-    contour: usize,
-    nested_to: &mut Option<usize>,
-) -> bool {
-    let mut cont = [false; 3];
-    let mut nto: [Option<usize>; 3] = [None; 3];
-    let step = outline.contours[contour].points.len() / 3;
-    cont[0] = ttf_outline_contour_info(outline, identifier, contour, step * 0, &mut nto[0]);
-    cont[1] = ttf_outline_contour_info(outline, identifier, contour, step * 1, &mut nto[1]);
-    cont[2] = ttf_outline_contour_info(outline, identifier, contour, step * 2, &mut nto[2]);
-    let mut index = None;
-    if nto[0].is_none() {
-        index = Some(0)
-    };
-    if nto[1].is_none() {
-        index = Some(1)
-    };
-    if nto[2].is_none() {
-        index = Some(2)
-    };
-    if index.is_none() {
-        index = if nto[0] == nto[1] || nto[0] == nto[2] {
-            Some(0)
-        } else {
-            Some(1)
-        };
-    }
-    *nested_to = nto[index.unwrap()];
-    return cont[index.unwrap()];
-}
-
-fn ttf_outline_contour_info(
-    outline: &Triangulator,
-    identifier: usize,
-    contour: usize,
-    test_point: usize,
-    nested_to: &mut Option<usize>,
-) -> bool {
-    let mut count = 0;
-    let mut nto = None;
-    let mut closest = 0.;
-    for i in 0..outline.contours.len() {
-        if i == contour {
-            continue;
-        }
-        if outline.contours[i].identifier != identifier {
-            continue;
-        }
-        let mut dist = 0.;
-        let res = ttf_outline_evenodd_base(
-            outline,
-            &outline.contours[contour].points[test_point],
-            i,
-            &mut dist,
-        );
-        count += res;
-        if (res & 1) == 0 {
-            continue;
-        };
-        if nto.is_none() || dist < closest {
-            closest = dist;
-            nto = Some(i);
-        }
-    }
-    *nested_to = nto;
-    return (count & 1) == 0;
-}
-
-fn ttf_outline_evenodd_base(
-    outline: &Triangulator,
-    point: &Vec2,
-    contour: usize,
-    dist: &mut f32,
-) -> i32 {
-    let mut counter = 0;
-    let mut closest_dx = 0.;
-
-    let points = &outline.contours[contour].points;
-    let len = outline.contours[contour].points.len();
-
-    let mut prev = &points[len - 1];
-    for pt in points.iter() {
-        let (u, b): (&Vec2, &Vec2);
-
-        if pt.y > prev.y {
-            u = pt;
-            b = prev;
-        } else {
-            u = prev;
-            b = pt;
-        }
-
-        if point.y <= u.y && point.y > b.y {
-            if point.x >= u.x || point.x >= b.x {
-                //            u
-                //      edge /
-                //          / <-- dx --> .
-                //         /            point
-                //        b
-                // calculation of distance from edge to point:
-                // y(x) = b.y + (u.y - b.y) / (u.x - b.x) * (x - b.x)    - the edge line equation
-                // x(y) = (y - b.y) / (u.y - b.y) * (u.x - b.x) + b.x    - inverse equation of line
-                // dx = point.x - x(point.y)                             - final equation for distance
-                let dy = u.y - b.y;
-                if dy.abs() > EPSILON
-                // horizontal edges are not handling
-                {
-                    let dx = point.x - (point.y - b.y) / dy * (u.x - b.x) - b.x;
-                    if dx >= 0. {
-                        if counter == 0 || dx < closest_dx {
-                            closest_dx = dx;
-                        }
-                        counter += 1;
-                    }
-                }
-            }
-        }
-        prev = pt;
-    }
-
-    *dist = closest_dx;
-
-    counter
-}
+use crate::{linked_list::LinkedListNode, vec2::Vec2, Triangulator, TriangulatorError, EPSILON};
 
 struct Vertex {
     pos: Vec2,
@@ -371,7 +174,7 @@ macro_rules! cramer2X2 {
     }};
 }
 
-struct Mesher {
+pub(crate) struct Mesher {
     /// Actual number of vertices (may be less than maxv)
     nv: usize,
     /// Vertex pool, length of maxv
@@ -403,7 +206,7 @@ struct MesherPinned {
 }
 
 impl Mesher {
-    fn new(o: &Triangulator) -> Self {
+    pub(crate) fn new(o: &Triangulator) -> Self {
         // Allocate memory and initialize fields
 
         let maxv = o.total_points;
@@ -564,7 +367,7 @@ impl Mesher {
         }
     }
 
-    fn process(
+    pub(crate) fn process(
         &mut self,
         deep: i32,
         optimize: bool,
@@ -1639,7 +1442,7 @@ impl Mesher {
         }
     }
 
-    fn triangles(&self) -> Vec<[Vec2; 3]> {
+    pub(crate) fn triangles(&self) -> Vec<[Vec2; 3]> {
         let mut vert: Vec<Vec2> = vec![];
         vert.reserve(self.nv);
         let mut triangles = vec![];
@@ -1899,12 +1702,6 @@ fn lines_has_common_point(l1p1: Vec2, l1p2: Vec2, l2p1: Vec2, l2p2: Vec2) -> boo
     let x0 = -b / k;
 
     return x0 > -EPSILON && x0 <= 1.0 + EPSILON;
-}
-
-#[derive(Debug, Snafu)]
-pub enum TriangulatorError {
-    Fail,
-    Incomplete,
 }
 
 fn compare_contours(o: &Triangulator, i1: usize, i2: usize) -> bool {
